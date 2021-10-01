71d6fac6e5bdc4f1c3c074c62b0a3afe97c45e84
Rahman Samadzade <56510976+capwan@users.noreply.github.com>
2021-10-01 14:13:43 +0400

    changed month from September to October 2021

--- main.py
+++ main.py
@@ -61,9 +61,9 @@ gui.doubleClick(870,299)
 gui.moveTo(846,381, duration=2)
 gui.doubleClick(846,381)
 
-# Choose "September 2021" folder and click on it
-gui.moveTo(841,473, duration=2)
-gui.doubleClick(841,473)
+# Choose "October 2021" folder and click on it
+gui.moveTo(845,475, duration=2)
+gui.doubleClick(845,475)
 
 # Create New folder
 gui.moveTo(1121,256, duration=2)
